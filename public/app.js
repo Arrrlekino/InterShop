9d57439e1b2d7498b57bb8d76a210a18354ee0e1
Alla Massorova <zebrasteel@gmail.com>
2020-04-13 19:04:18 +0200

    Added description

--- public/app.js
+++ public/app.js
@@ -1,4 +1,11 @@
 function something()
 	{
-		alert('Hello! ;-)');
+		//alert('Hello! ;-)');
+		/* этот код ниже для проверки переменной +-
+		*/
+		var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
+		x = x * 1 + 1; // x * 1 = для того чтобы х перевести в int из String
+
+		window.localStorage.setItem('bbb', x); // hh['bbb'] = x
+		alert(x); 
 	}
