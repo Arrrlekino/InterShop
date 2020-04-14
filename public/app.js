df1d9bd71c434fdce4a9bc8971ce962a444d8e08
Alla Massorova <zebrasteel@gmail.com>
2020-04-14 12:58:21 +0200

    Added hash to add_to_cart

--- public/app.js
+++ public/app.js
@@ -12,5 +12,8 @@ function something()
 
 	function add_to_cart(id)
 	{
-		alert('You add product with id:' + id);                                                                     
+		//alert('You add product with id:' + id);                                                                     
+		var x = window.localStorage.getItem('product_' + id);
+		x = x * 1 + 1; 
+		window.localStorage.setItem('product_' + id, x);
 	}
